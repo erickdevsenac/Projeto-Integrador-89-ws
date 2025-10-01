1b0f61e31d828e007d59218039bd7c52cff900d6
erickdevsenac <erickson50125857@edu.ms.senac.br>
2025-10-01 14:10:40 -0400

    resolve bug

--- core/serializers/PerfilSerializer.py
+++ core/serializers/PerfilSerializer.py
@@ -1,5 +1,5 @@
--from rest_framework import serializers
-from ..models import Perfil
+from rest_framework import serializers
+from core.models import Perfil
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
