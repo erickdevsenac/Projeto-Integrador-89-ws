bbcf07d5e88777c870c70e8ae990441eea5b67c5
MoneraCRVG <nicolas.almino@hotmail.com>
2025-10-10 14:35:53 -0400

    permitir que o vendedor crie cupons

--- core/views/admin.py
+++ core/views/admin.py
@@ -8,7 +8,7 @@ from core.forms import CupomForm
 def is_admin(user):
     return user.is_staff
 
-@user_passes_test(is_admin, login_url="/login/")
+# @user_passes_test(is_admin, login_url="/login/")
 def criar_cupom(request):
     form = CupomForm(request.POST or None)
     if form.is_valid():
