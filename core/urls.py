32c5f77e44bc699c3fcd1d4a82872ccacf163790
erickdevsenac <erickson50125857@edu.ms.senac.br>
2025-11-12 15:53:11 -0400

    corrige `urls.py` para passar id em `pacotes_detalhes`

--- core/urls.py
+++ core/urls.py
@@ -67,6 +67,7 @@ urlpatterns = [
     ),
     path("produto/cadastrar/", marketplace.cadastrar_produto, name="cadastrar_produto"),
     path("pacote/", public.pacote_detalhe, name="pacote_detalhe"),
+     path("pacote/<int:pacote_id>/", public.pacote_detalhe, name="pacote_detalhe"),
     
     # TODO:
     # path("categoria/<slug:categoria_slug>/", views.produtos_por_categoria, name="produtos_categoria"),
