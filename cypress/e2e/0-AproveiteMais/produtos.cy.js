f233eb221f42a86fae151efd28ff6ebbd1bfde29
LEILE46 <leile.bambil2705@gmail.com>
2026-01-15 17:06:37 -0400

    test:8 testes

---
+++ cypress/e2e/0-AproveiteMais/produtos.cy.js
@@ -0,0 +1,18 @@
+describe('testar se o caminho de produtos funciona', ()=> {
+    it('Testa se realmente da para fazer',()=> {
+        cy.visit('/login')
+
+        cy.get('.login-title').contains("Entrar")
+
+        cy.get('input[name="email"]').type("usuario1213@gmail.com")
+
+        cy.get('input[name="password"]').type('Senha123')
+
+        cy.contains("button", "Entrar").click()
+
+        cy.get('.main-title').contains("Bem-Vindo ao Aproveite +")
+        
+        cy.contains('a', 'Produtos').click()
+    })
+
+    })
